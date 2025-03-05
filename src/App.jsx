c5ff36c97e3fe6848b0124d64e3bc9d854ae6f6a
Samanvith <Samanvith2005@gmail.com>
2025-03-05 22:05:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-center text-4xl text-blue-500">
+        Hello world form react
+      </div>
+    </>
+  )
+}
+
+export default App
