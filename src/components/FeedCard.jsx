89ab63ae153c7b2b3121bd78862b2a5e2f632122
Samanvith <Samanvith2005@gmail.com>
2025-03-09 18:36:26 +0530

    Add EditProfile component and integrate with Profile; enhance FeedCard display

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -3,18 +3,29 @@ import React from "react";
 const FeedCard = ({ item }) => {
   const { name, profilePicture, age, gender, skills, about } = item;
   return (
-    <div className="border-2 border-gray-200  p-4 m-4 max-w-md mx-auto">
+    <div className="border-2 w-full  flex flex-col  border-gray-200  p-4 m-4 max-w-md mx-auto">
       <h1 className="text-center">{name}</h1>
       {
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgJCptPOx71EJH7cxh-m3JebMLah27zZgA7Ewl7hE6a0QpxLMhBsbrHx8&s"
+        profilePicture ?
+        (
+          <img
+          src={profilePicture}
           alt="profile"
           width="100"
           height="100"
         />
+        ) :(
+          <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgJCptPOx71EJH7cxh-m3JebMLah27zZgA7Ewl7hE6a0QpxLMhBsbrHx8&s"
+          alt="profile"
+          width="100"
+          height="100"
+          className="rounded-full w-40 h-30"
+        />
+        )
       }
-      {age && <h3>{age}</h3>}
-      {gender && <h3>{gender}</h3>}
+      {age && <h3 className="text-sm "> Age: {age}</h3>}
+      {gender && <h3> Gender: {gender}</h3>}
       {skills && (
         <div>
           {skills.map((skill) => {
@@ -23,7 +34,7 @@ const FeedCard = ({ item }) => {
         </div>
       )}
       {about && <h3>{about}</h3>}
-      <div className="flex justify-center items-center space-x-4">
+      {/* <div className="flex justify-center items-center space-x-4">
         <div>
           <button className="px-4 py-2 text-center bg-green-500 text-white rounded-md">
             Accept
@@ -34,7 +45,7 @@ const FeedCard = ({ item }) => {
             Reject
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
